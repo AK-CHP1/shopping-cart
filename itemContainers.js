95e7bbc11a8da1aa36e8452756e3f933f6dd1c7f
Aayush Kumar <aayushkumarfirst@gmail.com>
2022-11-28 13:25:04 +0530

    Added 'itemContainers.js`
    This file contains the implementation of `ItemContainer` which is
    a base class to represent an item from the available products in
    a specific context of the application.
    Then it has an implementation of `ItemCard` class which extends
    `ItemContainer` to represent an item on the shop index page.

---
+++ itemContainers.js
@@ -0,0 +1,58 @@
+/*
+This file contains class definitions for item containers
+which will control the behaviour of each item being displayed
+on the page. All these item containers will inherit from base
+`ItemContainer` class and provide methods to change their states
+or generating controls.
+*/
+
+class ItemContainer {
+    constructor(dom) {
+	this._dom = dom;
+	this._id = this._dom.dataset.productId;
+    }
+    get id() {
+	return this._id;
+    }
+    update(state) { }
+    generate_controls(state) {}
+}
+
+class ItemCard extends ItemContainer {
+    update(state) {
+	let qtyBox = this._dom.querySelector(".quantity");
+	let currentCount = this.state.getProductCount();
+	qtyBox.textContent = currentCount;
+    }
+    generate_controls(state) {
+	let addBtn = this._dom.querySelector("i.bi-plus-lg");
+	let remBtn = this._dom.querySelector("i.bi-dash-lg");
+	return [
+	    AddToCart(addBth, this, state),
+	    RemoveFromCart(remBtn, this, state)
+	];
+    }
+    static make_from_data(data) {
+	let shopCard = document.createElement("div");
+	shopCard.className = "item";
+	let {id, name, price, desc, img} = data;
+	shopCard.dataset.productId = id;
+	shopCard.innerHTML = `
+        <img src="${img}" width="220px" alt="${name}">
+        <div class="details">
+            <h3>${name}</h3>
+            <p>${desc}</p>
+            <div class="price-quantity">
+                <h2>$ ${price}</h2>
+                <div class="buttons">
+                    <i class="bi bi-dash-lg"></i>
+                    <div class="quantity">0</div>
+                    <i class="bi bi-plus-lg"></i>
+                </div>
+            </div>
+        </div>
+        `;
+
+	return ItemCard(shopCard);
+    }
+}
